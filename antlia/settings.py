d0d40cdf69907948d1162d01caeff82c10b68833
Geislor Crestani <geislor.crestani@siteblindado.com.br>
2016-06-18 15:11:00 -0300

    Settings: Altera linguagem

--- antlia/settings.py
+++ antlia/settings.py
@@ -103,8 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-BR'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
