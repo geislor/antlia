2fcbfbbf442eb2564bea10b775f1af082798d57b
Geislor Crestani <geislor@gmail.com>
2016-06-18 21:06:35 -0300

    slack with travis

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,6 +4,3 @@ from django.test import TestCase
 class CoreTestCase(TestCase):
     def test_core(self):
         self.assertTrue(True)
-
-    # def test_core_false(self):
-    #     self.assertTrue(False)
